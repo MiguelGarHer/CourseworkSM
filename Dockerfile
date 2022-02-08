15f2719e401f28458117f91131e569a36115c878
Vinh Phat Tu <vphatmail@gmail.com>
2022-02-08 15:43:18 +0000

    added comments and explanations to App.java and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
+# Selects OpenJDK as base image
 FROM openjdk:latest
+
+# Copy the jar to tmp directory
 COPY ./target/seMethods-0.1.0.2-jar-with-dependencies.jar /tmp
+
+# Set tmp directory as working directory
 WORKDIR /tmp
+
+# Main command of the image (runs the jar file)
 ENTRYPOINT ["java", "-jar", "seMethods-0.1.0.2-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,7 +9,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
+        // Connect to MongoDB on local system - we're connecting to the container mongo-dbserver using a network bridge
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
