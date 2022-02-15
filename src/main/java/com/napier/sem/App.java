0d60c4da2fec3b60e0d271d2f1b05acf1ccf8677
Vinh Phat Tu <vphatmail@gmail.com>
2022-02-15 15:31:38 +0000

    lab3 done with mysql

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,7 +4,15 @@ import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args)
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -17,9 +25,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +36,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +48,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -58,5 +67,69 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public Country getCountry(String name)
+    {
+        Country country = new Country();
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT code, name, continent, population\n" +
+                            "FROM country\n" +
+                            "WHERE name = " + "'" + name + "'" +
+                            ";";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                country.code = rset.getString("code");
+                country.name = rset.getString("name");
+                country.continent = rset.getString("continent");
+                country.population = rset.getInt("population");
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+        return country;
+
+    }
+
+    public void displayCountry(Country country) {
+        if (country != null) {
+            System.out.println(country.code);
+            System.out.println(country.name);
+            System.out.println(country.continent);
+            System.out.println(country.population);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // Create new App object
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Retrieve employee with employee id: 255530
+        Country country = a.getCountry("Aruba");
+
+        // Display employee
+        a.displayCountry(country);
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,22 @@
+package com.napier.sem;
+
+public class Country {
+    public Country(){
+    }
+
+    public String code;
+    public String name;
+    public String continent;
+    public String region;
+    public String surfaceArea;
+    public int indepYear;
+    public int population;
+    public double lifeExpectancy;
+    public double GNP;
+    public double GNPOld;
+    public String localName;
+
+    //etc
+
+
+}
