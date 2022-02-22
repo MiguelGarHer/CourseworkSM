790fdd0a363f4abbf00f8764d4af40d1f7c7f512
Haidi Yu <yvhaidi@outlook.com>
2022-02-22 12:48:09 +0000

    Update Country class

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -5,18 +5,20 @@ public class Country {
     }
 
     public String code;
-    public String name;
-    public String continent;
-    public String region;
-    public String surfaceArea;
-    public int indepYear;
-    public int population;
-    public double lifeExpectancy;
-    public double GNP;
-    public double GNPOld;
-    public String localName;
-
-    //etc
+	public String name;
+	public String continent;
+	public String region;
+	public String surfaceArea;
+	public int indepYear;
+	public int population;
+	public double lifeExpectancy;
+	public double GNP;
+	public double GNPOld;
+	public String localName;
+	public String GovermentForm;
+	public String HeadofState;
+	public int Capital;
+	public string Code2;
 
 
 }
