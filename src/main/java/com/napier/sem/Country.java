3d69a5325d1204b8709ed5c338c9e2ffa0d75ecf
Vinh Phat Tu <vphatmail@gmail.com>
2022-02-22 16:17:45 +0000

    getCountries working

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -5,7 +5,7 @@ public class Country {
 	public String name;
 	public String continent;
 	public String region;
-	public String surfaceArea;
+	public double surfaceArea;
 	public int indepYear;
 	public int population;
 	public double lifeExpectancy;
@@ -17,8 +17,42 @@ public class Country {
 	public int capital;
 	public String code2;
 
-	public Country() {
+	public Country(String code, String name, String continent, String region, double surfaceArea, int indepYear, int population, double lifeExpectancy, double GNP, double GNPOld, String localName, String governmentForm, String headOfState, int capital, String code2) {
+		this.code = code;
+		this.name = name;
+		this.continent = continent;
+		this.region = region;
+		this.surfaceArea = surfaceArea;
+		this.indepYear = indepYear;
+		this.population = population;
+		this.lifeExpectancy = lifeExpectancy;
+		this.GNP = GNP;
+		this.GNPOld = GNPOld;
+		this.localName = localName;
+		this.governmentForm = governmentForm;
+		this.headOfState = headOfState;
+		this.capital = capital;
+		this.code2 = code2;
 	}
 
-	
+	@Override
+	public String toString() {
+		return "Country{" +
+				"code='" + code + '\'' +
+				", name='" + name + '\'' +
+				", continent='" + continent + '\'' +
+				", region='" + region + '\'' +
+				", surfaceArea=" + surfaceArea +
+				", indepYear=" + indepYear +
+				", population=" + population +
+				", lifeExpectancy=" + lifeExpectancy +
+				", GNP=" + GNP +
+				", GNPOld=" + GNPOld +
+				", localName='" + localName + '\'' +
+				", governmentForm='" + governmentForm + '\'' +
+				", headOfState='" + headOfState + '\'' +
+				", capital=" + capital +
+				", code2='" + code2 + '\'' +
+				'}';
+	}
 }
--- src/main/java/com/napier/sem/World.java
+++ src/main/java/com/napier/sem/World.java
@@ -1,13 +1,135 @@
 package com.napier.sem;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class World {
 
-    /// ArrayList containing all the data of the countries
-    private ArrayList<Country> countries;
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
 
+    /// ArrayList containing all the data of the countries
+    private ArrayList<Country> countries = new ArrayList<>();
+
+    /**
+     *
+     */
     public World(){
+        // Connect to MySQL
+        connect();
+
+        // Read all countries
+        getCountries();
+
+        // Test read country
+        displayCountry(countries.get(0));
+
+        // Disconnect from MySQL
+        disconnect();
+
+    }
+
+
+    /**
+     * Connect to the MySQL database.
+     */
+    private void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(15000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    private void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    /**
+     * Read all countries from database
+     */
+    private void getCountries()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT *" +
+                    "FROM country;";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            while (rset.next())
+            {
+                Country country = new Country(rset.getString(1),rset.getString(2),rset.getString(3),rset.getString(4),rset.getDouble(5),rset.getInt(6),rset.getInt(7),rset.getDouble(8),rset.getDouble(9),rset.getDouble(10),rset.getString(11),rset.getString(12),rset.getString(13),rset.getInt(14),rset.getString(15));
+                countries.add(country);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to retrieve country details");
+        }
+    }
+
+    /**
+     *
+     * @param country
+     */
+    public void displayCountry(Country country) {
+        if (country != null) {
+            System.out.println(country);
+        }
     }
 
     /**
