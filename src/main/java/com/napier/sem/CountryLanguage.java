0b80d2f26eab427593ea99eeebe260b5c016120f
Eduardo <edumidon16@gmail.com>
2022-02-22 12:19:19 +0000

    Adding classes(CountryLanguage and city)

---
+++ src/main/java/com/napier/sem/CountryLanguage.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+public class CountryLanguage {
+    public CountryLanguage(){
+    }
+    public String countryCode;
+    public String Language;
+    public boolean isOficial;
+    public double percentage;
+}
