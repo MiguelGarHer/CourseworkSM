9b0a63c8bc06b6e2c97c969a42181ad290ec2a33
MiguelGarHer <40494773@live.napier.ac.uk>
2022-02-22 12:19:23 +0000

    Implemented the world class

---
+++ src/main/java/com/napier/sem/World.java
@@ -0,0 +1,188 @@
+package com.napier.sem;
+
+import java.util.ArrayList;
+
+public class World {
+
+    /// ArrayList containing all the data of the countries
+    private ArrayList<Country> countries;
+
+    World(){
+    }
+
+    /**
+     *
+     * @param countryName
+     */
+    public void getCountryRep(String countryName){
+    }
+
+    /**
+     *
+     * @param cityName
+     */
+    public void getCityReport(String cityName){
+    }
+
+    /**
+     *
+     * @param capCityName
+     */
+    public void getCapCityRep(String capCityName){
+    }
+
+    /**
+     *
+     */
+    public void getPopulationRep(){
+    }
+
+    /**
+     *
+     */
+    public void sortCountriesPopWorld(){
+    }
+
+    /**
+     *
+     * @param continentName
+     */
+    public void sortCountriesPopContinent(String continentName){
+    }
+
+    /**
+     *
+     * @param regionName
+     */
+    public void sortCountriesPopRegion(String regionName){
+    }
+
+    /**
+     *
+     */
+    public void nPopCountriesWorld(){
+    }
+
+    /**
+     *
+     * @param continentName
+     */
+    public void nPopCountriesContinent(String continentName){
+    }
+
+    /**
+     *
+     * @param regionName
+     */
+    public void nPopCountriesRegion(String regionName){
+    }
+
+    /**
+     *
+     */
+    public void sortCitiesPopWorld(){
+    }
+
+    /**
+     *
+     * @param continentName
+     */
+    public void sortCitiesPopContinent(String continentName){
+    }
+
+    /**
+     *
+     * @param regionName
+     */
+    public void sortCitiesPopRegion(String regionName){
+    }
+
+    /**
+     *
+     * @param countryName
+     */
+    public void sortCitiesPopCountry(String countryName){
+    }
+
+    /**
+     *
+     * @param districtName
+     */
+    public void sortCitiesPopDistrict(String districtName){
+    }
+
+    /**
+     *
+     */
+    public void nPopCitiesWorld(){
+
+    }
+
+    /**
+     *
+     * @param continentName
+     */
+    public void nPopCitiesContinent(String continentName){
+    }
+
+    /**
+     *
+     * @param regionName
+     */
+    public void nPopCitiesRegion(String regionName){
+    }
+
+    /**
+     *
+     * @param countryName
+     */
+    public void nPopCitiesCountry(String countryName){
+    }
+
+    /**
+     *
+     * @param districtName
+     */
+    public void nPopCitiesDistrict(String districtName){
+    }
+
+    /**
+     *
+     */
+    public void sortCapCitiesPopWorld(){
+    }
+
+    /**
+     *
+     * @param regionName
+     */
+    public void sortCapCitiesPopRegion(String regionName){
+    }
+
+    /**
+     *
+     * @param continentName
+     */
+    public void sortCapCitiesPopContinent(String continentName){
+    }
+
+    /**
+     *
+     * @param regionName
+     */
+    public void nPopCapCitiesRegion(String regionName){
+    }
+
+    /**
+     *
+     * @param continentName
+     */
+    public void nPopCapCitiesContinent(String continentName){
+    }
+
+    /**
+     *
+     */
+    public void nPopCapCitiesWorld(){
+    }
+}
