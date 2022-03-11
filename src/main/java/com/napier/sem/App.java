e9569c5450453f6a1070d2c50a92dcc2dee0511d
Vinh Phat Tu <vphatmail@gmail.com>
2022-03-11 17:39:30 +0000

    lab 7 done

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -15,6 +15,29 @@ public class App
         // Read all countries
         world.getCountries();
 
+        // Vinh: Test: sortCitiesPopWorld():
+        world.sortCitiesPopWorld();
+
+        System.out.println();
+
+        // Vinh: Test: sortCitiesPopContinent()
+        world.sortCitiesPopContinent("Asia");
+
+        System.out.println();
+
+        // Vinh: Test: sortCitiesPopDistrict()
+        world.sortCitiesPopDistrict("California");
+
+        System.out.println();
+
+        //Miguel: Test: sortCitiesPopRegion()
+        world.sortCitiesPopRegion("Caribbean");
+
+        System.out.println();
+
+        //Haidi: Test: sortCitiesPopCountry()
+        world.sortCitiesPopCountry("France");
+
         // Disconnect from MySQL
         world.disconnect();
     }
