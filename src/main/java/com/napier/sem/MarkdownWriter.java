fdaa39c456cfa50e0cc09f7a919e6fbcef962701
Vinh Phat Tu <vphatmail@gmail.com>
2022-04-04 17:35:29 +0100

    populationReportToMarkdown officially done

--- src/main/java/com/napier/sem/MarkdownWriter.java
+++ src/main/java/com/napier/sem/MarkdownWriter.java
@@ -131,7 +131,7 @@ public class MarkdownWriter {
                 name + " | " +
                 cityPopulation + " (" + cityPopulationPercentage + "%) | " +
                 countrySidePopulation + " (" + countrySidePopulationPercentage  + "%) | " +
-                totalPopulation + " (100%) | " +
+                totalPopulation + " (" + cityPopulationPercentage + countrySidePopulationPercentage + "%) |" +
                 "\r\n");
         try {
             new File("./reports/").mkdir();
